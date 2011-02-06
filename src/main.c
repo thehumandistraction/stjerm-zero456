35a8a14dcc93da7fd411e4bceaaf5626e028b5cc
Thomas Pifer <zero456@gmail.com>
2011-02-05 22:06:46 -0500

    Begin the long and arduous task of porting stjerm to cmake.

--- src/main.c
+++ src/main.c
@@ -24,6 +24,7 @@
 
 #include <gtk/gtk.h>
 #include "stjerm.h"
+#include "version.h" //version.h is created by cmake.
 
 int sargc;
 char **sargv;
@@ -96,7 +97,7 @@ void print_version(void) {
            "License GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl.html>\n"
            "This is free software: you are free to change and redistribute it.\n"
            "There is NO WARRANTY, to the extent permitted by law.\n", 
-           STJERM_VERSION);
+           version);
 }
 
 int main(int argc, char *argv[]) {
